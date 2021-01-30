3145a2238b17a92dbc83ec913f5ef735b89e068a
Isinya <isabellabdoamaral@usp.br>
2021-01-30 11:15:51 -0300

    Suporte à outros pronomes Marcação do tempo de execução do loop

--- nhom.py
+++ nhom.py
@@ -1,12 +1,30 @@
 from random import *
+import time
 
-nome = input("Qual o nome da sua gatinya? ")
+gender = input("Qual o pronome do seu catto? ").lower()
+
+if gender in "ele dele e".split():
+    genderNoun = "do seu gatinyo? "
+    pronoun = "ele"
+elif gender in "ela dela a".split():
+    genderNoun = "da sua gatinya? "
+    pronoun = "ela"
+else:
+    genderNoun = "de seu gatinye? "
+    pronoun = "elx"
+
+nome = input("Qual o nome " + genderNoun)
 
 alt = [(i.lower(), i.upper()) for i in "n h o m".split()]
 
 alt.append(("", " ", "\n"))
 
-for i in range(int(input("Quantos nhomnhom ela merece? "))):
+nhomCount = int(input("Quantos nhomnhom " + pronoun + " merece? "))
+
+start_time = time.time()
+for i in range(nhomCount):
     if i == 0:
         print(nome)
     print("".join(map(choice,alt)), end="")
+
+print("--- %s seconds ---" % (time.time() - start_time))
